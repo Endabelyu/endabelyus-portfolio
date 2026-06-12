53de4215275813f32f03ecfe6b501e661e49a870
Naswa Wilantama <endabelyuproject@gmail.com>
2026-06-12 11:51:46 +0700

    chore: use www.endabelyu.com as canonical domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://endabelyu.com',
+  site: 'https://www.endabelyu.com',
   vite: {
     plugins: [tailwindcss()]
   },
